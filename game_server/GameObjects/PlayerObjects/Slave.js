84bede0965777d4dec566661f7311d749bd2b993
Harsh Patel <coderharshpatel@gmail.com>
2016-02-06 16:31:30 -0500

    Add weapons to slave class

--- game_server/GameObjects/PlayerObjects/Slave.js
+++ game_server/GameObjects/PlayerObjects/Slave.js
@@ -10,12 +10,39 @@ function Slave() {
 
     }
 
+    this.weaponList = [
+  {
+      "name": "Shackles",
+      "description": "Old shackles, perfect for strangling.",
+      "att": 5
+  },
+  {
+      "name": "Whip",
+      "description": "His master's old whip.",
+      "att": 10
+  },
+  {
+      "name": "Chains",
+      "description": "The chains his family was tied up with.",
+      "att": 15
+  }
+    ];
+
     this.levelUp = function(){
         this.baseStats.att += 5;
         this.baseStats.hp += 5;
         this.baseStats.def += 5;
         this.baseStats.agi += 5;
     }
+
+    this.upgradeWeapon = function() {
+
+    }
+
+
+
+
+
 }
 
 module.exports = new Slave();
