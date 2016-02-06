8cae1ae47265490a2988d0f4fe95fdd047942b81
Vuk Petrovic <vukpetrovic1@yahoo.com>
2016-02-06 02:22:53 -0500

    TerrainHandler progress

--- game_server/GameObjects/TerrainObjects/Terrain.js
+++ game_server/GameObjects/TerrainObjects/Terrain.js
@@ -8,6 +8,16 @@ function TerrainHandler() {
     this.gridSize = 100; //good default
     this.playerPos = null;
 
+    /*
+      import all the different terrains
+    */
+    this.Cave = require('./Cave.js');
+    this.Forest = require('./Forest.js');
+    this.Town = require('./Town.js');
+    this.Desert = require('./Desert.js');
+    this.Castle = require('./Castle.js');
+    this.Sea = require('./Sea.js');
+    
     //initialize terrain-related game states 
     this.init = function() {
 	//intialize the grid spaces
@@ -46,7 +56,24 @@ function TerrainHandler() {
 	if(this.isValidSlot(x, y)) {
 	    var terrainSelector = Math.floor(Math.random() * 6);
 	    switch(terrainSelector) {
-		//to do
+	    case 0: this.grid[x][y] = new this.Forest();
+		this.grid[x][y].init();
+		break;
+      	    case 1: this.grid[x][y] = new this.Cave();
+		this.grid[x][y].init();
+		break;
+	    case 2: this.grid[x][y] = new this.Town();
+		this.grid[x][y].init();
+		break;
+	    case 3: this.grid[x][y] = new this.Desert();
+		this.grid[x][y].init();
+		break;
+	    case 4: this.grid[x][y] = new this.Castle();
+		this.grid[x][y].init();
+		break;
+	    case 5: this.grid[x][y] = new this.Sea();
+		this.grid[x][y].init();
+		break;
 	    }
 	}
 	return false; //FAIL
@@ -56,6 +83,12 @@ function TerrainHandler() {
     this.isValidSlot = function(x, y) {
 	return (x < 0 || y < 0 || x >= this.gridSize || y >= this.gridSize || isNaN(x) || isNaN(y)) ? false : true;
     }
+
+    //checks to see if this terrain slot has been instantiated
+    this.exists = function(x, y) {
+	return (this.grid[x][y] == null) ? false : true;
+    }
+    
 }
 
 module.exports = new TerrainHandler();
