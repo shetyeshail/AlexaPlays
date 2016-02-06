af0b848d9a6566de5d3e7b9497e178a5c6ef9a0d
Vuk Petrovic <vukpetrovic1@yahoo.com>
2016-02-06 00:02:56 -0500

    Setup boilerplate for GameHandler

---
+++ game_server/GameObjects/GameHandler.js
@@ -0,0 +1,11 @@
+
+/*
+  GameHandler is the object that deals with all of the game handling - sets up interaction 
+  between the user and game environment; as well as communicartion between other game objects.
+*/
+
+function GameHandler() {
+    
+}
+
+module.exports = new GameHandler();
